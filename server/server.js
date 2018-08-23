7248561938e2ba410e56336ad0407f12a479a7c3
Ed Robinson <edrobinson@eds-macbook.home>
2018-08-23 22:29:37 +0100

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 
+var {ObjectID} = require('mongodb');
+
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -30,6 +32,23 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(req.params.id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  })
+  .catch((e) => {
+    console.log(e);
+    res.status(400).send();
   });
 });
 
