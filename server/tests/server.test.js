a34331d807c0c62d89748da6c08a978a59093b08
Ed Robinson <edrobinson@Eds-MacBook.lan>
2018-08-27 22:13:34 +0100

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -111,3 +111,42 @@ describe('GET /todos/:id', () => {
     .end(done);
   })
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    request(app)
+    .delete(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      Todo.findById(todos[0]._id.toHexString())
+      .then((todo) => {
+        expect(todo).toBeFalsy();
+        done();
+      })
+      .catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var randomObjectID = new ObjectID();
+    request(app)
+    .delete(`/todos/${randomObjectID.toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+    .delete('/todos/123')
+    .expect(404)
+    .end(done);
+  })
+
+});
